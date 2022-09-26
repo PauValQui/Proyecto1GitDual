43fa6d595e81778a718f8281116cad06370b3e31
Paulavq <91205671+PauValQui@users.noreply.github.com>
2022-09-26 13:13:51 +0200

    Update App.java

--- src/main/java/com/everis/mvn_pvq/App.java
+++ src/main/java/com/everis/mvn_pvq/App.java
@@ -9,6 +9,6 @@ public class App
 {	
     public static void main( String[] args )
     {
-        System.out.println(StringUtils.isAllLowerCase("abd") );
+        System.out.println(StringUtils.isAllLowerCase("abdjasdks") );
     }
 }
